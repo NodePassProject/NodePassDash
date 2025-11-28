30a18f44940bcab138d457f3c17a383f4fbc4396
Mecozea <mecozea@gmail.com>
2025-11-28 16:19:42 +0800

    feat: 导入主控时支持拖拽修改导入的顺序； update: 更新负载均衡placeHolder文案
    (cherry picked from commit 3ef5ec8b418ddab47feaa0f1250ad40fb1af10cf)

--- web/src/components/services/service-create-modal.tsx
+++ web/src/components/services/service-create-modal.tsx
@@ -790,7 +790,7 @@ export default function ScenarioCreateModal({
                 </Tooltip>
               </div>
             }
-            placeholder="192.168.1.1&#10;192.168.1.2"
+            placeholder="192.168.1.1:80&#10;192.168.1.2:80"
             minRows={2}
             value={formData.extendTargetAddresses}
             onValueChange={(v) => handleField("extendTargetAddresses", v)}
@@ -979,7 +979,7 @@ export default function ScenarioCreateModal({
                 </Tooltip>
               </div>
             }
-            placeholder="192.168.1.1&#10;192.168.1.2"
+            placeholder="192.168.1.1:80&#10;192.168.1.2:80"
             minRows={2}
             value={formData.extendTargetAddressesSingle}
             onValueChange={(v) => handleField("extendTargetAddressesSingle", v)}
@@ -1244,7 +1244,7 @@ export default function ScenarioCreateModal({
                 </Tooltip>
               </div>
             }
-            placeholder="192.168.1.1&#10;192.168.1.2"
+            placeholder="192.168.1.1:80&#10;192.168.1.2:80"
             minRows={2}
             value={formData.extendTargetAddresses2}
             onValueChange={(v) => handleField("extendTargetAddresses2", v)}
